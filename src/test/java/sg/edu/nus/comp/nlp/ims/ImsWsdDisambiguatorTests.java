a4a0520aa05f6123e1d9a3a73da7eb47da41db8d
Sander Puts <putssander@gmail.com>
2017-05-07 17:44:21 +0200

    restructured imports and relocated unittest

--- src/test/java/sg/edu/nus/comp/nlp/ims/ImsWsdDisambiguatorTests.java
+++ src/test/java/sg/edu/nus/comp/nlp/ims/ImsWsdDisambiguatorTests.java
@@ -1,30 +1,12 @@
-/**
- * Copyright 2016
- * Ubiquitous Knowledge Processing (UKP) Lab
- * Technische Universität Darmstadt
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-import static org.junit.Assert.assertEquals;
+package sg.edu.nus.comp.nlp.ims;
 
 import org.junit.Ignore;
 import org.junit.Test;
 import sg.edu.nus.comp.nlp.ims.implement.CTester;
 
-import java.util.ArrayList;
-
+/**
+ * Test if maven dependencies are compatible
+ */
 public class ImsWsdDisambiguatorTests {
 
     private static final String LEMMA = "0";
